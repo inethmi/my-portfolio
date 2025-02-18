70993b21f76f3a463b5bff03855eb8f7a0a00ce1
Inuri  Ediriweera <inu048ediriweera@gmail.com>
2025-02-18 16:05:04 +0530

    AboutMe section updated

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ const AboutMe = () => {
     <section id="about" className="about-me">
       {/* Photo Container (Left Side) */}
       <div className="photo-container">
-        <img src={`${process.env.PUBLIC_URL}/inuri.jpg`} alt="Inuri Ediriveera" />
+        <img src={`${process.env.PUBLIC_URL}/inuri.jpg`} alt="Inuri Ediriweera" />
       </div>
 
       {/* About Details (Right Side) */}
@@ -17,16 +17,16 @@ const AboutMe = () => {
         </h4>
 
         <p>
-          <strong>Name:</strong> Inuri Ediriveera
+          <strong>Name:</strong> Inuri Ediriweera
         </p>
         <p>
           <strong>Birthday:</strong> 24 May 2000
         </p>
         <p>
-          <strong>Age:</strong> 24 years
+          <strong>Age:</strong> 25 years
         </p>
         <p>
-          <strong>Degree:</strong> Bsc Hons in Software Engineering (Undergraduate) SLIIT
+          <strong>Degree:</strong> Bsc(Hons) in Information Technology (Undergraduate) SLIIT
         </p>
         <p>
           <strong>Languages:</strong> English, Sinhala
