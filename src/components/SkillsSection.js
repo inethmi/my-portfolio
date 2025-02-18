c96be2a5ae9229288b346534aed890a9053c1742
Inuri  Ediriweera <inu048ediriweera@gmail.com>
2025-02-18 15:41:53 +0530

    updated the skills section as 2 columns

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -10,67 +10,79 @@ const SkillsSection = () => {
 
         {/* Skills List */}
         <div className="skills-list">
-          <div className="skill">
-            <h3>HTML</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "90%" }}></div>
+          <div className="left-side">
+            <div className="skill">
+              <h3>HTML</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "90%" }}></div>
+              </div>
             </div>
-          </div>
 
-          <div className="skill">
-            <h3>CSS</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "85%" }}></div>
+            <div className="skill">
+              <h3>CSS</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "85%" }}></div>
+              </div>
             </div>
-          </div>
 
-          <div className="skill">
-            <h3>JavaScript</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "80%" }}></div>
+            <div className="skill">
+              <h3>JavaScript</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "80%" }}></div>
+              </div>
             </div>
-          </div>
 
-          <div className="skill">
-            <h3>React</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "75%" }}></div>
+            <div className="skill">
+              <h3>React</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "75%" }}></div>
+              </div>
             </div>
-          </div>
 
-          <div className="skill">
-            <h3>ASP.NET Core</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "55%" }}></div>
-            </div>
-          </div>
-
-          <div className="skill">
-            <h3>C#</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "50%" }}></div>
-            </div>
-          </div>
-
-          <div className="skill">
-            <h3>Git</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "60%" }}></div>
+            <div className="skill">
+              <h3>ASP.NET Core</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "55%" }}></div>
+              </div>
             </div>
           </div>
 
 
-          <div className="skill">
-            <h3>SQL</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "50%" }}></div>
+          <div className="right-side">
+            <div className="skill">
+              <h3>C#</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "50%" }}></div>
+              </div>
             </div>
-          </div>
 
-          <div className="skill">
-            <h3>iQuery / Bootstrap</h3>
-            <div className="skill-bar">
-              <div className="skill-level" style={{ width: "50%" }}></div>
+            <div className="skill">
+              <h3>Git</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "60%" }}></div>
+              </div>
+            </div>
+
+
+            <div className="skill">
+              <h3>SQL</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "50%" }}></div>
+              </div>
+            </div>
+
+            <div className="skill">
+              <h3>iQuery / Bootstrap</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "50%" }}></div>
+              </div>
+            </div>
+
+            <div className="skill">
+              <h3>Power BI</h3>
+              <div className="skill-bar">
+                <div className="skill-level" style={{ width: "45%" }}></div>
+              </div>
             </div>
           </div>
 
